c895f29aaedb43e69064508c2f3f0f2a741315f2
ZVNexus <ZVNexus@outlook.com>
2021-08-06 18:38:43 +0530

    phoenix: Disable the usage of ConfigStore.
     * As per commit LineageOS/android_hardware_interfaces@11a113a ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -206,6 +206,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0 \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
